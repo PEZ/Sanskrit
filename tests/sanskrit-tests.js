4766531f73dfe44e60b4d0689796bf118c9c5fcd
Olle Dahlström <olle.dahlstrom@gmail.com>
2012-01-31 13:20:24 +0530

    Initial jstestrunner tests setup

---
+++ tests/sanskrit-tests.js
@@ -0,0 +1,11 @@
+/*jslint browser: true, indent: 4 */
+/*global TestCase, assertEquals*/
+TestCase("Test of the test", {
+    setUp: function () {
+        
+    },
+    testBaseUrlConfig: function () {
+        assertEquals(1, 1);
+    }
+});
+	
